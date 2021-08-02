c306bd0995b5275b9968f5386a5cc883aa196384
Iain Watt <iainskwatt@gmail.com>
2021-08-02 21:23:15 +0100

    added some factory function practice.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,51 @@
-alert("Hello World!");
+//Factory function
+
+const personFactory = (name, age) => {
+    const sayHello = () => console.log(`Hello, ${name}!`);
+    return {name, age, sayHello }; //note after shorthand no need for 'name: name, age: age'
+};
+
+const jeff = personFactory('jeff', 27);
+
+console.log(jeff.name); // 'jeff'
+
+jeff.sayHello(); // calls the function and logs 'hello!'
+
+// Same as above but done with constructor pattern
+
+const Person = function(name, age) {
+    this.sayHello = () => console.log('hello!');
+    this.name = name;
+    this.age = age;
+};
+
+const pete = new Person('pete', 28);
+
+
+const name = "Maynard";
+const color = "red";
+const number = 34;
+const food = "rice";
+
+console.log(name, color, number, food);
+
+console.log({name, color, number, food});
+
+
+const myFunction = function () {
+    console.log(this); // this = global, [object Window]
+  };
+myFunction();
+  
+const myObject = {};
+myObject.myMethod = function () {
+  console.log(this); // this = Object { myObject }
+};
+
+const nav = document.querySelector('.nav'); // <nav class="nav">
+const toggleNav = function () {
+  console.log(this); // this = <nav> element
+};
+nav.addEventListener('click', toggleNav, false);
+
+
