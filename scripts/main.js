6f3ad5d43722677f21a054e40119c90784ccb54d
Iain Watt <iainskwatt@gmail.com>
2021-08-02 21:40:11 +0100

    worked on more public/private factory functions using a game like object

--- scripts/main.js
+++ scripts/main.js
@@ -64,4 +64,31 @@ taco.printString(); // this prints "----TACO----"
 */
 
 
+const Player = (name, level) => {
+    let health = level * 2;
+    const getLevel = () => level;
+    const getName = () => name;
+    const die = () => {
+        console.log('die!');
+    };
+    const damage = x => {
+        health -= x;
+        if (health <= 0) {
+            die();
+        }
+    };
+    const attack = enemy => {
+        if (level < enemy.getLevel()) {
+            damage(1);
+            console.log(`${enemy.getName()} has damaged ${name}`);
+        }
+        if (level >= enemy.getLevel()) {
+            enemy.damage(1);
+            console.log(`${name} has damaged ${enemy.getName()}`);
+        }
+    };
+    return {attack, damage, getLevel, getName}
+};
+const jimmie = Player('jim', 10);
+const badGuy = Player('jeff', 5);
 
